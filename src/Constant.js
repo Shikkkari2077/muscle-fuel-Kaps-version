b3d1c24ba23630bef59aadcb89ce7ee995182464
unknown <kapilprajapati061@gmail.com>
2022-04-26 20:10:35 +0530

    Change

--- src/Constant.js
+++ src/Constant.js
@@ -1,11 +1,11 @@
 module.exports = {
     getAPI: function () {
-      // return "https://testv0.musclefuel-kw.com/ws"
-      return "https://musclefuel-kw.com/musclefuel/ws"
+      return "https://testv0.musclefuel-kw.com/ws"
+      // return "https://musclefuel-kw.com/musclefuel/ws"
     },
 
     getPaymentAPI: function () {
-            return "https://musclefuel-kw.com/musclefuel/"
+            // return "https://musclefuel-kw.com/musclefuel/"
           }
   };
 
